291ef1f1bd220493404e9ab1bfe5407c2425dc85
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2024-11-16 23:16:15 +0330

    actix middleware worked!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use crate_template_lynx::main as lynx_main;
+// use crate_template_lynx::main as lynx_main;
 
-fn main() {
-    lynx_main();
+// fn main() {
+//     lynx_main();
+// }
+
+use your_middleware_crate_name::run_server;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let url = "127.0.0.1:8080"; // Address to bind the server
+
+    run_server(url).await
 }
