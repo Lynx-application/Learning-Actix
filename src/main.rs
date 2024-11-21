4e3a9899b3b25a501512251f947c1ddd71b61820
60204 <60204@PROGRAMING-REZA>
2024-11-21 16:30:37 +0330

    GET and POST method implemented!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use actix_web::{App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder};
 
+#[get("/")]
 async fn index() -> impl Responder {
     "Hello, Actix!"
+}
+
+#[get("/greet/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello,{}!", name)
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    format!("Recieved: {}", req_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", actix_web::web::get().to(index)) // Basic route
+            .service(index) // root route (Main)
+            .service(greet) // greet route (GET)
+            .service(echo) // echo route (POST)
     })
-    .bind("127.0.0.1:8080")? // Bind to localhost on port 8080
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
